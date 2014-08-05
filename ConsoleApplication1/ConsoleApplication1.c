a4cd65813bc21b81362c713cf3a77ccbf4f879f4
dechnostick <etherealmaro@gmail.com>
2014-08-05 21:52:37 +0900

    リテラルをやめる

--- ConsoleApplication1/ConsoleApplication1.c
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -20,20 +20,19 @@ typedef struct tagIClass1 {
 	MyVTable *lpVtbl;
 } _IClass1;
 
-const GUID CLSID_Class1 = 
-{ 0xC3A7E3DE, 0x8C77, 0x301C, { 0xB5, 0x23, 0x7F, 0x53, 0x78, 0x82, 0x28, 0x69 } };
-
-const GUID IID_Class1 = 
-{ 0xC2D5D4CA, 0x0EAC, 0x3671, { 0x9C, 0xE8, 0xE5, 0x1E, 0x18, 0x9D, 0x0B, 0xC7 } };
-
+CLSID clsid;
 _IClass1* pIClass1;
 
 int main(int argc, char *argv[])
 {
 	HRESULT hResult;
+
+	OleInitialize(NULL);
+	CLSIDFromProgID(L"CommonVB.SpreadsheetGearWrapper", &clsid);
+
 	CoInitialize(0);
-	hResult = CoCreateInstance(&CLSID_Class1, 0, CLSCTX_INPROC_SERVER, &IID_Class1, (void**)&pIClass1);
-	
+	hResult = CoCreateInstance(&clsid, 0, CLSCTX_INPROC_SERVER, &IID_IUnknown, (void**)&pIClass1);
+
 	if (!SUCCEEDED(hResult)) {
 		printf("ERROR: %08x\n", hResult);
 		return 0;
@@ -61,7 +60,7 @@ int main(int argc, char *argv[])
 
 	pIClass1->lpVtbl->Test((void*)pIClass1, bstr_in, &bstr_out);
 
-	printf("%S ", (WCHAR *)bstr_out);
+	printf("%S\n", (WCHAR *)bstr_out);
 
 	SysFreeString(bstr_in);
 	SysFreeString(bstr_out);
